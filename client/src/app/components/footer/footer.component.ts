49dfe6c12bc30a49b42e26838e7b140dc39fcbad
Reza Taba <mrtabaa@gmail.com>
2024-08-16 18:51:34 +0200

    Set layout of header, main and footer

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule, MatDividerModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,14 @@ import { Component, inject, Signal } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonService } from '../../services/common.service';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    MatToolbarModule, MatButtonModule
+    MatToolbarModule, MatButtonModule, MatDividerModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
