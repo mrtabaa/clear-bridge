fde2f9a8ba1e0b83644e8ca02e8f58c569ae3832
Reza Taba <mrtabaa@gmail.com>
2024-08-20 21:50:44 +0200

    Finished content, layout and style

--- client/src/app/components/about-us/about-us.component.ts
+++ client/src/app/components/about-us/about-us.component.ts
@@ -2,13 +2,16 @@ import { Component, inject, OnInit, Signal } from '@angular/core';
 import { CommonService } from '../../services/common.service';
 import { RouterModule } from '@angular/router';
 import { NavArrowsComponent } from '../navbars/nav-arrows/nav-arrows.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
   imports: [
-    RouterModule,
-    NavArrowsComponent
+    RouterModule, NgOptimizedImage,
+    NavArrowsComponent,
+    MatDividerModule
   ],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
