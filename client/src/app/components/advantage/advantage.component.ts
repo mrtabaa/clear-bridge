31a0dca03dbf8c9c2c401c728461c6c2a5f548e7
Reza Taba <mrtabaa@gmail.com>
2024-08-22 16:51:21 +0200

    Finished advantage

--- client/src/app/components/advantage/advantage.component.ts
+++ client/src/app/components/advantage/advantage.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonService } from '../../services/common.service';
 import { NavArrowsComponent } from '../navbars/nav-arrows/nav-arrows.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-advantage',
   standalone: true,
   imports: [
+    NgOptimizedImage,
     NavArrowsComponent
   ],
   templateUrl: './advantage.component.html',
