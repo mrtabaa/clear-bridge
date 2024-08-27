f38a414304208e06a48e167f16328dde5d5c50d5
Reza Taba <mrtabaa@gmail.com>
2024-08-27 20:35:26 +0200

    Added Google maps link to location and made all icons clickable

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    MatToolbarModule, MatDividerModule
+    MatToolbarModule, MatDividerModule, MatIconModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
