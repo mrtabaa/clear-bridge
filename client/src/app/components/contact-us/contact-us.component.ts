ebb809523cc14d40c6d98c53cf190d2a23293170
Reza Taba <mrtabaa@gmail.com>
2024-09-02 18:29:41 +0300

    Added loading while jotform loading

--- client/src/app/components/contact-us/contact-us.component.ts
+++ client/src/app/components/contact-us/contact-us.component.ts
@@ -5,13 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { GoogleMapsComponent } from '../dialogs/google-maps/google-maps.component';
 import { NavbarComponent } from '../navbars/navbar/navbar.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
   imports: [
     NavbarComponent, NavArrowsComponent,
-    MatIconModule, MatDialogModule
+    MatIconModule, MatDialogModule, MatProgressSpinnerModule
   ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
@@ -19,6 +20,7 @@ import { NavbarComponent } from '../navbars/navbar/navbar.component';
 export class ContactUsComponent implements OnInit {
   private _commonService = inject(CommonService);
   private readonly _dialog = inject(MatDialog);
+  isJotformLoaded = false;
 
   ngOnInit(): void {
     this._commonService.pageTitleSig.set('CONTACT US');
@@ -26,5 +28,9 @@ export class ContactUsComponent implements OnInit {
 
   openDialog() {
     this._dialog.open(GoogleMapsComponent);
+  }
+
+  onJotformLoad(): void {
+    this.isJotformLoaded = true;
   }
 }
